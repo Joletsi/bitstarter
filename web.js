f0bdc81310dbebc9f163351e0fdc277afab0686b
Joletsi <joletsi@gmail.com>
2013-07-10 21:49:25 +0000

    read from index.html working

--- web.js
+++ web.js
@@ -5,13 +5,13 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
-    var buffer = new Buffer('');
-    
-    var fileContents = fs.readFile('index.html');
-    var bytes = buffer.write(fileContents);
-    var message = buffer.toString('buffer',0,bytes);
+    buffer = new Buffer(100);
+    var fileContents = fs.readFileSync('index.html');
+    var message = fileContents.toString();
+    bytes = buffer.write('fileContents');
+//    var message = buffer.toString(buffer,0,bytes);
+//    response.send(message);
     response.send(message);
-
 });
 
 var port = process.env.PORT || 5000;
